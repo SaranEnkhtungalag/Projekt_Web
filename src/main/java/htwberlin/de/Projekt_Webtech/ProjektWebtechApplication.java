3a7d39ff2e160a3a72dbfced2eb48741a94e2d44
SaranEnkhtungalag <tungaa.love61@gmail.com>
2023-04-17 16:52:31 +0200

    test

---
+++ src/main/java/htwberlin/de/Projekt_Webtech/ProjektWebtechApplication.java
@@ -0,0 +1,13 @@
+package htwberlin.de.Projekt_Webtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektWebtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektWebtechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htwberlin/de/Projekt_Webtech/ProjektWebtechApplicationTests.java
@@ -0,0 +1,13 @@
+package htwberlin.de.Projekt_Webtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektWebtechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
