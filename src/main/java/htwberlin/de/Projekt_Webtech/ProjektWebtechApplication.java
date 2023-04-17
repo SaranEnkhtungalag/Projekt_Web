85200aba7e26f3806752a5b7814d13b1e5363bd4
SaranEnkhtungalag <tungaa.love61@gmail.com>
2023-04-17 17:04:33 +0200

    helloworld

--- src/main/java/htwberlin/de/Projekt_Webtech/ProjektWebtechApplication.java
+++ src/main/java/htwberlin/de/Projekt_Webtech/ProjektWebtechApplication.java
@@ -3,6 +3,8 @@ package htwberlin.de.Projekt_Webtech;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.swing.*;
+
 @SpringBootApplication
 public class ProjektWebtechApplication {
 
