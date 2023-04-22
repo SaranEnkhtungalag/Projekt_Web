8d6e1554442dc1518d2e5866be49de1f47520c8f
SaranEnkhtungalag <tungaa.love61@gmail.com>
2023-04-22 16:58:41 +0200

    Projekt Beschreibung

--- src/main/java/htwberlin/de/Projekt_Webtech/HelloController.java
+++ src/main/java/htwberlin/de/Projekt_Webtech/HelloController.java
@@ -1,14 +1,21 @@
 package htwberlin.de.Projekt_Webtech;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
-    @GetMapping("/greeting")
+    @GetMapping("/greeting/")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name) {
         return "Hello, " + name;
     }
 
+    @GetMapping("/path/param/{name}/{part2}")
+    public String pathparam(@PathVariable(name = "name") String nam, @PathVariable(name = "part2") String par2) {
+        return "hello, " + nam+par2;
+    }
+
+    @GetMapping("/path/param")
+    public String pathparam(@RequestHeader(value="User-Agent") String par2) {
+        return "hello, " + par2;
+    }
 }
